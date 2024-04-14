cff34056f204f38d5fff6e15fc56433bb89275a1
Ramy Adel <81263757+Ramyadel2300@users.noreply.github.com>
2024-04-14 16:17:38 +0200

    Changing Logo and Revit tab name

--- RevitExcelIntegrationApp/RevitHandlers/ExternalApplication.cs
+++ RevitExcelIntegrationApp/RevitHandlers/ExternalApplication.cs
@@ -8,17 +8,14 @@ namespace RevitExcelIntegrationApp.RevitHandlers
 {
     public class ExternalApplication : IExternalApplication
     {
-        public Result OnShutdown(UIControlledApplication application)
-        {
-            return Result.Succeeded;
-        }
+        public Result OnShutdown(UIControlledApplication application) => Result.Succeeded;
 
         public Result OnStartup(UIControlledApplication application)
         {
-            application.CreateRibbonTab("ExcelIntegration");
+            application.CreateRibbonTab("Blue Construction");
             string path = Assembly.GetExecutingAssembly().Location;
-            RibbonPanel MyPanel = application.CreateRibbonPanel("ExcelIntegration", "Revit-Excel Integration");
-            PushButtonData InsertPricesFromExcelButton = new PushButtonData("Button1", "R-Excel", path, "RevitExcelIntegrationApp.RevitHandlers.ExternalCommand");
+            RibbonPanel MyPanel = application.CreateRibbonPanel("Blue Construction", "Revit-Excel Pricing Integration");
+            PushButtonData InsertPricesFromExcelButton = new PushButtonData("Button1", "RevLynx", path, "RevitExcelIntegrationApp.RevitHandlers.ExternalCommand");
             string outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
             string IconPath = Path.Combine(outPutDirectory, "Images\\ButtonIcon.png");
             InsertPricesFromExcelButton.LargeImage = new BitmapImage(new Uri(IconPath));
